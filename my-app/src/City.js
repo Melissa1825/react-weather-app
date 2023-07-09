77ac718c902914ff58075be6771b4591145e2e90
Melissa1825 <114811994+Melissa1825@users.noreply.github.com>
2023-07-09 13:57:52 -0400

    Added font awesome icons

--- my-app/src/City.js
+++ my-app/src/City.js
@@ -1,10 +1,17 @@
 import "./index.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 export default function City() {
   return (
     <div className="locationIcon">
-      <i className="fa-solid fa-location-dot"></i>
-      <h4>Toronto</h4>
+        <div>
+            <FontAwesomeIcon className="fa-solid" icon={faLocationDot} />
+        </div>
+        <div>
+            <h4>Toronto</h4>
+        </div>
     </div>
   );
 }
--- my-app/src/Details.js
+++ my-app/src/Details.js
@@ -1,5 +1,9 @@
 import "./index.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWind } from '@fortawesome/free-solid-svg-icons'
+import { faDroplet } from '@fortawesome/free-solid-svg-icons'
+
 export default function Details() {
   return (
     <div className="details">
@@ -8,7 +12,7 @@ export default function Details() {
           <div className="col-4"></div>
           <div className="col-2">
             <li>
-              <i className="fa-solid fa-wind"></i>
+            <FontAwesomeIcon className="fa-solid" icon={faWind} />
               <span>5</span> <span>km/h</span>
               <p>Wind</p>
             </li>
@@ -16,7 +20,7 @@ export default function Details() {
 
           <div className="col-2">
             <li>
-              <i className="fa-solid fa-droplet"></i>
+            <FontAwesomeIcon className="fa-solid" icon={faDroplet} />
               <span>70</span> <span>%</span>
               <p>Humidity</p>
             </li>
